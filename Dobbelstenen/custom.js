2b7f20bb32ea416ae4b4afae2464832962fc1444
Sean <44732696+TheSeanU@users.noreply.github.com>
2020-02-12 16:25:52 +0100

    Update custom.js

--- Dobbelstenen/custom.js
+++ Dobbelstenen/custom.js
@@ -4,4 +4,8 @@ function dobbelStenen (){
   for (var i = 0; i < dob.length; i++) {
     dob[i].innerText = Math.floor((Math.random() * 6) + 1);
   }
+  var sortDob = [...document.getElementsByClassName('dob')].map(e=>e.innerHTML).sort();
+    sortDob
+  console.log(sortDob);
 }
+dobbelStenen();
