a3e0d9ffc8f7774826658c6e95e824ff3c33fee0
Sean <44732696+TheSeanU@users.noreply.github.com>
2020-02-17 13:25:08 +0100

    Update custom.js

--- Dobbelstenen/custom.js
+++ Dobbelstenen/custom.js
@@ -1,11 +1,30 @@
-const dob = document.getElementsByClassName('dob');
-
 function dobbelStenen (){
+  var dob = document.getElementsByClassName('dob');
   for (var i = 0; i < dob.length; i++) {
     dob[i].innerText = Math.floor((Math.random() * 6) + 1);
   }
-  var sortDob = [...document.getElementsByClassName('dob')].map(e=>e.innerHTML).sort();
-    sortDob
+  var count = {};
+  var sortDob = [...document.getElementsByClassName('dob')].map(e => e.innerHTML).sort();
+  for(var j = 0; j < sortDob.length; j++){
+    var num = sortDob[j];
+     count[num] = count[num] ? count[num] + 1 : 1;
+     if (count === undefined){
+       return ["0"]
+     }
+   }
+   var result = Object.keys(count).map(function (key) {
+     return[count[key], Number(key)];
+  })
+
+  var dobResult = count;
+  for (var i = 0; i < dobResult.length; i++) {
+    document.getElementsByClassName('result')[0].innerHTML = dobResult[i];
+
+  }
+  console.log(dobResult);
+  console.log(result);
+  console.log(count);
   console.log(sortDob);
 }
+
 dobbelStenen();
