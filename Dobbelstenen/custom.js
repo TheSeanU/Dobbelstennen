1fffaab0bc5063cba754c599a225652af4b9c2a0
Sean <44732696+TheSeanU@users.noreply.github.com>
2020-02-18 16:45:30 +0100

    Update custom.js

--- Dobbelstenen/custom.js
+++ Dobbelstenen/custom.js
@@ -1,30 +1,29 @@
 function dobbelStenen (){
   var dob = document.getElementsByClassName('dob');
+
   for (var i = 0; i < dob.length; i++) {
-    dob[i].innerText = Math.floor((Math.random() * 6) + 1);
+    dob[i].innerHTML = Math.floor((Math.random() * 6) + 1);
   }
-  var count = {};
+
   var sortDob = [...document.getElementsByClassName('dob')].map(e => e.innerHTML).sort();
-  for(var j = 0; j < sortDob.length; j++){
-    var num = sortDob[j];
+  var count = {};
+
+  for(var i = 0; i < sortDob.length; i++){
+    var num = sortDob[i];
      count[num] = count[num] ? count[num] + 1 : 1;
-     if (count === undefined){
-       return ["0"]
-     }
+   }
+   if (count[num] === 0) {
+    return 0;
    }
    var result = Object.keys(count).map(function (key) {
-     return[count[key], Number(key)];
-  })
+       return [" " + count[key] + " x " + Number(key)];
+   });
 
-  var dobResult = count;
-  for (var i = 0; i < dobResult.length; i++) {
-    document.getElementsByClassName('result')[0].innerHTML = dobResult[i];
-
-  }
-  console.log(dobResult);
-  console.log(result);
-  console.log(count);
-  console.log(sortDob);
+   var prResult = document.getElementsByClassName('result');
+   for (var i = 0; i < prResult.length; i++) {
+     prResult[i].innerHTML = result;
+     console.log(result);
+   }
 }
 
 dobbelStenen();
